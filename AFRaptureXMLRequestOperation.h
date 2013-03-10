f7e4d678aff702940794d4f1af4689bfc08826a6
Jan Sanchez <janfsd+github@gmail.com>
2013-03-10 17:06:25 -0400

    Added License & updated to ARC, tweaks

--- AFRaptureXMLRequestOperation.h
+++ AFRaptureXMLRequestOperation.h
@@ -2,12 +2,33 @@
 //  AFRaptureXMLRequestOperation.h
 //
 //  Created by Jan Sanchez on 5/13/12.
-//  Copyright (c) 2012 Jan Sanchez. All rights reserved.
+//  Copyright (c) 2013 Jan Sanchez (http://jansanchez.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import <Foundation/Foundation.h>
 #import "AFHTTPRequestOperation.h"
 #import "RXMLElement.h"
+
+#import <Availability.h>
 
 /**
  `AFRaptureXMLRequestOperation` is a subclass of `AFHTTPRequestOperation` for downloading and working with XML response data.
@@ -33,9 +54,9 @@
 ///----------------------------
 
 /**
- A RXMLElement constructed from the response data.
+ A RXMLElement constructed from the response data. If an error occurs while parsing, `nil` will be returned, and the `error` property will be set to the error.
  */
-@property (readonly, nonatomic, retain) RXMLElement *responseXMLElement;
+@property (readonly, nonatomic, strong) RXMLElement *responseXMLElement;
 
 /**
  Creates and returns an `AFRaptureXMLRequestOperation` object and sets the specified success and failure callbacks.
@@ -47,7 +68,7 @@
  @return A new XML request operation
  */
 + (AFRaptureXMLRequestOperation *)XMLParserRequestOperationWithRequest:(NSURLRequest *)urlRequest
-                                                        success:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, RXMLElement *XMLElement))success
-                                                        failure:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, RXMLElement *XMLElement))failure;
+                                                               success:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, RXMLElement *XMLElement))success
+                                                               failure:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, RXMLElement *XMLElement))failure;
 
 @end
